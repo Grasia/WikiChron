7dc852bb5c632dc3452d7658bcca5a1841a55b4d
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2017-11-14 17:10:16 +0100

    Wikis dropdown working

--- main.py
+++ main.py
@@ -16,8 +16,16 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
+from dash.dependencies import Input, Output
 
-wikis = ['recipes', 'eslagunanegra_pages_full', 'cocktails', 'zelda']
+wikis = ['eslagunanegra_pages_full', 'cocktails', 'zelda']
+wikis_df = []
+
+metrics = ['edits_monthly', 'users_new']
+
+data_edits_pages_monthly = []
+data_new_users_monthly = []
+
 
 def get_data_from_csv(csv):
     print('Loading csv for ' + csv)
@@ -33,12 +41,7 @@ def get_data_from_csv(csv):
 
 def generate_graphs():
 
-    wikis_data = []
-    for wiki in wikis:
-        wikis_data.append(get_data_from_csv(wiki))
-
-    data_edits_pages_monthly = []
-    for i, data in enumerate(wikis_data):
+    for i, data in enumerate(wikis_df):
         monthly_data = data.groupby(pd.Grouper(key='timestamp',freq='M'))
         edits = monthly_data.page_id.count()
         data_edits_pages_monthly.append(
@@ -50,10 +53,9 @@ def generate_graphs():
         )
 
     monthly_users = []
-    for data in wikis_data:
+    for data in wikis_df:
         monthly_users.append(data.drop_duplicates('contributor_id'))
 
-    data_new_users_monthly = []
     for i,wiki in enumerate(wikis):
         monthly_new_users = monthly_users[i].groupby(pd.Grouper(key='timestamp',freq='M')).contributor_id.count()
         data_new_users_monthly.append(
@@ -89,6 +91,16 @@ def generate_graphs():
 
 
 def generate_main_content():
+
+    wikis_dropdown_options = []
+    for index, wiki in enumerate(wikis):
+        wikis_dropdown_options.append({'label': wiki, 'value': index})
+
+    metrics_dropdown_options = []
+    for index, metric in enumerate(metrics):
+        metrics_dropdown_options.append({'label': metric, 'value': index})
+
+
     return html.Div(id='main',
         style={'width': '100%'},
         children=[
@@ -114,14 +126,10 @@ def generate_main_content():
                             dcc.Dropdown(
                                 id='wikis-selection-dropdown',
                                 className='seven columns',
-                                options=[
-                                    {'label': 'Wikipedia', 'value': '1'},
-                                    {'label': 'Wiki 4', 'value': '2'},
-                                    {'label': 'Wiki 5', 'value': '3'}
-                                ],
+                                options=wikis_dropdown_options,
                                 multi=True,
                                 searchable=False,
-                                value="1,2,3"
+                                value=[ option['value'] for option in wikis_dropdown_options ]
                              ),
                          ]),
                     ]),
@@ -135,15 +143,10 @@ def generate_main_content():
                             dcc.Dropdown(
                                 id='metrics-selection-dropdown',
                                 className='seven columns',
-                                options=[
-                                    {'label': 'Total per user', 'value': '1'},
-                                    {'label': 'Metric 3', 'value': '2'},
-                                    {'label': 'Metric 4', 'value': '3'},
-                                    {'label': 'Metric 5', 'value': '4'}
-                                ],
+                                options=metrics_dropdown_options,
                                 multi=True,
                                 searchable=False,
-                                value="1,2,3,4"
+                                value=[ option['value'] for option in metrics_dropdown_options ]
                             ),
                          ])
              ]),
@@ -159,11 +162,12 @@ def generate_main_content():
 
                         dcc.RangeSlider(
                             id='dates-slider',
-                            count=1,
                             min=-5,
                             max=10,
-                            step=0.5,
-                            value=[-3, 7]
+                            step=None,
+                            value=[0, 7],
+                            disabled=True
+                            #~ marks={str(year): str(year) for year in df['year'].unique()}
                         )
                    ]),
 
@@ -171,42 +175,51 @@ def generate_main_content():
         ]
     );
 
-#@app.callback(
-    #dash.dependencies.Output('graph-with-slider', 'figure'),
-    #[dash.dependencies.Input('year-slider', 'value')])
-#def update_figure(selected_year):
-    #filtered_df = df[df.year == selected_year]
-    #traces = []
-    #for i in filtered_df.continent.unique():
-        #df_by_continent = filtered_df[filtered_df['continent'] == i]
-        #traces.append(go.Scatter(
-            #x=df_by_continent['gdpPercap'],
-            #y=df_by_continent['lifeExp'],
-            #text=df_by_continent['country'],
-            #mode='markers',
-            #opacity=0.7,
-            #marker={
-                #'size': 15,
-                #'line': {'width': 0.5, 'color': 'white'}
-            #},
-            #name=i
-        #))
-
-    #return {
-        #'data': traces,
-        #'layout': go.Layout(
-            #xaxis={'type': 'log', 'title': 'GDP Per Capita'},
-            #yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
-            #margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-            #legend={'x': 0, 'y': 1},
-            #hovermode='closest'
-        #)
-    #}
 
 
 if __name__ == '__main__':
     app = dash.Dash()
     app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
     app.css.append_css({"external_url": "https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css"})
+
+    for wiki in wikis:
+        wikis_df.append(get_data_from_csv(wiki))
+
     app.layout = generate_main_content()
+
+    @app.callback(
+        Output('graphs', 'children'),
+        [Input('wikis-selection-dropdown', 'value')])
+    def update_graphs(selected_wikis):
+
+        for i in range(len(wikis)):
+            if i in selected_wikis:
+                data_edits_pages_monthly[i]['visible'] = True
+                data_new_users_monthly[i]['visible'] = True
+            else:
+                data_edits_pages_monthly[i]['visible'] = "legendonly"
+                data_new_users_monthly[i]['visible'] = "legendonly"
+
+        return html.Div([
+                dcc.Graph(
+                    id='graph-1',
+                    figure={
+                        'data': data_edits_pages_monthly,
+                        'layout': {
+                            'title': 'Monthly edits'
+                        }
+                    }
+                ),
+                dcc.Graph(
+                    id='graph-2',
+                    figure={
+                        'data': data_new_users_monthly,
+                        'layout': {
+                            'title': 'Monthly new users'
+                        }
+                    }
+                )],
+            id='graphs'
+        )
+
     app.run_server(debug=True, port=8053)
