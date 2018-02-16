af31a1ce1383cca68fa8d6642fb337310905b0a1
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2018-02-16 13:56:33 +0100

    Better code styling

--- lib/metrics/stats.py
+++ lib/metrics/stats.py
@@ -19,12 +19,14 @@ MINIMAL_USERS_PERCENTIL_MAX_5 = 100
 MINIMAL_USERS_PERCENTIL_MAX_10 = 50
 MINIMAL_USERS_PERCENTIL_MAX_20 = 25
 
+
 def calculate_index_all_months(data):
     monthly_data = data.groupby(pd.Grouper(key='timestamp', freq='MS'))
     index = monthly_data.size().index
     return index
 
 # Pages
+
 
 def pages_new(data, index):
     # We use the fact that data is sorted first by page_title and them by revision_id
@@ -36,8 +38,10 @@ def pages_new(data, index):
         series = series.reindex(index, fill_value=0)
     return series
 
+
 def pages_accum(data, index):
     return (pages_new(data, index).cumsum())
+
 
 def pages_main_new(data, index):
     pages = data.drop_duplicates('page_id')
@@ -47,8 +51,10 @@ def pages_main_new(data, index):
         series = series.reindex(index, fill_value=0)
     return series
 
+
 def pages_main_accum(data, index):
     return (pages_main_new(data, index).cumsum())
+
 
 def pages_edited(data, index):
     monthly_data = data.groupby([pd.Grouper(key='timestamp', freq='MS')])
@@ -57,9 +63,10 @@ def pages_edited(data, index):
         series = series.reindex(index, fill_value=0)
     return series
 
+
 def main_edited(data, index):
     main_pages = data[data['page_ns'] == 0]
-    monthly_data = main_pages.groupby([pd.Grouper(key='timestamp',freq='MS')])
+    monthly_data = main_pages.groupby([pd.Grouper(key='timestamp', freq='MS')])
     series = monthly_data.apply(lambda x: len(x.page_id.unique()))
     if index is not None:
         series = series.reindex(index, fill_value=0)
@@ -69,26 +76,32 @@ def main_edited(data, index):
 
 # Editions
 
+
 def edits(data, index):
-    monthly_data = data.groupby(pd.Grouper(key='timestamp',freq='MS'))
+    monthly_data = data.groupby(pd.Grouper(key='timestamp', freq='MS'))
     series = monthly_data.size()
     if index is not None:
         series = series.reindex(index, fill_value=0)
     return series
 
+
 def edits_accum(data, index):
     return (edits(data, index).cumsum())
+
 
 def edits_main_content(data, index):
     edits_main_data = data[data['page_ns'] == 0]
     return (edits(edits_main_data, index))
 
+
 def edits_main_content_accum(data, index):
     return (edits_main_content(data, index).cumsum())
+
 
 def edits_article_talk(data, index):
     edits_talk_data = data[data['page_ns'] == 1]
     return (edits(edits_talk_data, index))
+
 
 def edits_user_talk(data, index):
     edits_talk_data = data[data['page_ns'] == 3]
@@ -98,41 +111,48 @@ def edits_user_talk(data, index):
 
 # Users
 
+
 def users_active(data, index):
-    monthly_data = data.groupby(pd.Grouper(key='timestamp',freq='MS'))
+    monthly_data = data.groupby(pd.Grouper(key='timestamp', freq='MS'))
     series = monthly_data.apply(lambda x: len(x.contributor_id.unique()))
     if index is not None:
         series = series.reindex(index, fill_value=0)
     return series
 
+
 def users_new(data, index):
     users = data.drop_duplicates('contributor_id')
-    series = users.groupby(pd.Grouper(key='timestamp',freq='MS')).size()
+    series = users.groupby(pd.Grouper(key='timestamp', freq='MS')).size()
     if index is not None:
         series = series.reindex(index, fill_value=0)
     return series
+
 
 def users_accum(data, index):
     return (users_new(data, index).cumsum())
 
+
 def users_new_anonymous(data, index):
     users = data.drop_duplicates('contributor_id')
     anonymous_users = users[users['contributor_name'] == 'Anonymous']
-    series = anonymous_users.groupby(pd.Grouper(key='timestamp',freq='MS')).size()
+    series = anonymous_users.groupby(pd.Grouper(key='timestamp', freq='MS')).size()
     if index is not None:
         series = series.reindex(index, fill_value=0)
     return series
+
 
 def users_anonymous_accum(data, index):
     return (users_new_anonymous(data, index).cumsum())
 
+
 def users_new_registered(data, index):
     users = data.drop_duplicates('contributor_id')
     non_anonymous_users = users[users['contributor_name'] != 'Anonymous']
-    series =  non_anonymous_users.groupby(pd.Grouper(key='timestamp',freq='MS')).size()
+    series = non_anonymous_users.groupby(pd.Grouper(key='timestamp', freq='MS')).size()
     if index is not None:
         series = series.reindex(index, fill_value=0)
     return series
+
 
 def users_registered_accum(data, index):
     return (users_new_registered(data, index).cumsum())
@@ -141,14 +161,18 @@ def users_registered_accum(data, index):
 
 # Ratios
 
+
 def edits_per_users_accum(data, index):
     return (edits_accum(data, index) / users_accum(data, index))
+
 
 def edits_per_users_monthly(data, index):
     return (edits(data, index) / users_active(data, index))
 
+
 def edits_per_pages_accum(data, index):
     return (edits_accum(data, index) / pages_accum(data, index))
+
 
 def edits_per_pages_monthly(data, index):
     return (edits(data, index) / pages_edited(data, index))
@@ -158,6 +182,7 @@ def edits_per_pages_monthly(data, index):
 # Distribution Of Work
 
 ##### Helper functions #####
+
 
 def contributions_per_author(data):
     """
@@ -184,7 +209,7 @@ def calc_ratio_percentile_max(data, index, percentile, minimal_users):
 
     percentage = percentile * 0.01
     i = 0
-    monthly_data = data.groupby(pd.Grouper(key='timestamp',freq='MS'))
+    monthly_data = data.groupby(pd.Grouper(key='timestamp', freq='MS'))
     result = pd.Series(index=monthly_data.size().index)
     indices = result.index
     accum_data = pd.DataFrame()
@@ -207,6 +232,7 @@ def calc_ratio_percentile_max(data, index, percentile, minimal_users):
     return result
 
 ##### callable ditribution metrics #####
+
 
 def gini_accum(data, index):
 
