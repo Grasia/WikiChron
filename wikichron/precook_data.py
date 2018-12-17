888f3df0e2a94f3b391114aa4ab684366591229f
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2018-12-17 19:23:07 +0100

    Several changes in precook_data.py.
    Renamed script to precook_data.py, moved inside wikichron/ dir and use data_dir/ instead of source_path

--- wikichron/precook_data.py
+++ wikichron/precook_data.py
@@ -19,33 +19,33 @@
 import os
 from os.path import join
 import sys
-import wikichron.lib.interface as lib
 import pandas as pd
 import json
 import time
 
-from wikichron.lib.networks.types.CoEditingNetwork import CoEditingNetwork
+import lib.interface as lib
+from lib.networks.types.CoEditingNetwork import CoEditingNetwork
 
-source_path = os.path.join('.', 'data')
-dest_path = os.path.join('.', 'precooked_data', 'networks')
+if not 'WIKICHRON_DATA_DIR' in os.environ:
+    os.environ['WIKICHRON_DATA_DIR'] = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../data')
+data_dir = os.environ['WIKICHRON_DATA_DIR']
+
+dest_path = os.path.join('precooked_data', 'networks')
 
 def main(*args):
 
-    if not os.path.isdir(source_path):
+    if not os.path.isdir(data_dir):
         print('Error: the path "{}" was not found'.format(source_path))
         return 1
 
-
     if not os.path.isdir(dest_path):
         print(f'Creating bin directory for first time: {dest_path}')
-        print(dest_path)
         os.makedirs(dest_path)
 
-
-    wikis = json.load(open(os.path.join(lib.data_dir, 'wikis.json')))
+    wikis = json.load(open(os.path.join(data_dir, 'wikis.json')))
 
     for wiki in wikis:
-        df = lib.get_dataframe_from_csv(wiki['data'])
+        df = lib.get_dataframe_from_csv(os.path.join(data_dir, wiki['data']))
         lib.prepare_data(df)
         df = lib.clean_up_bot_activity(df, wiki)
 
@@ -61,7 +61,6 @@ def main(*args):
             o_f = wiki['data'][:-3]
             o_f = '{}{}.bin'.format(o_f, net.code)
             net.write_pickle(fname = os.path.join(dest_path, o_f))
-        sys.exit(0)
 
     return 0
 
