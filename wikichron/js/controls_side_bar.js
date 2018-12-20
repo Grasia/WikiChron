4dbe00318bb2d066cf7041113cf6db3fa026fabb
Abel Serrano Juste <akronix5@gmail.com>
2018-12-20 19:22:54 +0100

    start with left sidebar hidden by default when controls_side_bar is loaded

--- wikichron/js/controls_side_bar.js
+++ wikichron/js/controls_side_bar.js
@@ -1,6 +1,6 @@
 function setEvents() {
     console.log('Setting js events for controls_side_bar.py...');
-    hideSideBar(); // start with controls hidden by default
+    hideSideBar(); // start with left sidebar hidden by default when controls_side_bar is loaded
     document.getElementById('controls-fold-img-container').onclick = hideControlsSideBar;
     console.log('All Set!');
 }
