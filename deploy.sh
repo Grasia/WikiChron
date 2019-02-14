62023b844183d94c95dcd483ffdb9a8ea2e34961
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2019-02-14 20:04:20 +0100

    Deployment script improved

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-(git pull origin master && pkill -HUP gunicorn) || (gunicorn wikichron:server -c gunicorn_config.py)
+#!/bin/bash
+git pull origin master &&
+if [ -e venv.old/ ]; then
+  rm -r venv.old/
+fi  
+mv venv/ venv.old/ &&
+virtualenv -p python3 venv/ &&
+source venv/bin/activate &&
+pip install -r requirements.txt &&
+gunicorn wikichron:server -c gunicorn_config.py
