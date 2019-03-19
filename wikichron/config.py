afe905327f85284113d53f244f8fe523910db600
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2019-03-19 19:05:29 +0100

    Forgot to update port in DevelopmentConfig

--- wikichron/config.py
+++ wikichron/config.py
@@ -1,7 +1,7 @@
 from wikichron.dash.dash_config import DevelopmentConfig as DashDevelopmentConfig
 
 class DevelopmentConfig(DashDevelopmentConfig):
-    PORT = '5000'
+    PORT = '5001'
     APP_HOSTNAME = f'localhost:{PORT}'
     DASH_STANDALONE = False
 
