15a16ebfc0f43d35d807d9a9a3749fec7750b54f
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2019-05-26 16:21:32 +0200

    Creating Dockerfile

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-pip install -r requirements.txt
+#apt-get install -y build-essential python-dev libxml2 libxml2-dev zlib1g-dev && # deps for python3-igraph
+apt-get install -y python3-igraph && # deps for python3-igraph
+pip3 install -r requirements.txt
